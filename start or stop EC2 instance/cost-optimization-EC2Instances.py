dfe8a47b5cc30159e4823e4c785cff03aead1e0b
Nithyalakshmi Murugan <146036380+Nithya-95@users.noreply.github.com>
2025-11-28 13:43:08 +0000

    Implement EC2 instance start/stop functionality

---
+++ start or stop EC2 instance/cost-optimization-EC2Instances.py
@@ -0,0 +1,21 @@
+import boto3
+def lambda_handler(event, context):
+    action=event['action']
+    ec2 = boto3.client('ec2')
+    response = ec2.describe_instances(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+    instances = []      
+    for Reservation in response['Reservations']:
+        for instance in Reservation['Instances']:
+            instances.append(instance['InstanceId'])
+            print(instance['InstanceId'])
+            for instanceid in instances:
+                if action == 'stop':
+                    ec2.stop_instances(InstanceIds=[instanceid])
+                    print('stopped your instances: ' + str(instances))
+                elif action == 'start':
+                    ec2.start_instances(InstanceIds=[instanceid])
+                    print('started your instances: ' + str(instances))
+                else:
+                    print('invalid action')
+
+     
